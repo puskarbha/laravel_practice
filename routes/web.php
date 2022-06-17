dd6d39eee77b4dc043e180bae6b641cd4c435127
Puskar <puskarbha@gmail.com>
2022-06-17 19:41:29 +0545

    Eloquent basic

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -36,59 +36,79 @@ Route::any('/test',function(){
 
 // Route:get('/{name}',function($name){
 //     return view('home');
-// });
- Route::get('/example',function(){
-     return view('welcome');
- });
-
- Route::get('/about',function(){
-    echo"<h1>Welcome to about us page.</h1>Know more about us";
- });
-
-//  Route::get('/contact',function(){
-//      echo"<h2>Our contact details are given below. </h2> Mobile:9812348456";
+// // });
+//  Route::get('/example',function(){
+//      return view('welcome');
 //  });
 
-//  Route::get('/post/{id}',function($id){
-//     echo"Well,our id is : ".$id;
+//  Route::get('/about',function(){
+//     echo"<h1>Welcome to about us page.</h1>Know more about us";
 //  });
 
- Route::get('admin/posts/example',array('as'=>'admin.home',function(){
-     $url =route('admin.home');
-     return"this url is ".$url;
- }));
+// //  Route::get('/contact',function(){
+// //      echo"<h2>Our contact details are given below. </h2> Mobile:9812348456";
+// //  });
 
- Route::get('nepal/nawalprashi/basa',array('as'=>'puskar.home',function(){
-    $address=route('puskar.home');
-    return"puskar lives in".$address." municipality";
- }));
+// //  Route::get('/post/{id}',function($id){
+// //     echo"Well,our id is : ".$id;
+// //  });
 
-//  Route::get('admin/post/linkExample',array('as'=>'page.link',function(){
-//     <a href="route('page.link')">Click Here</a>
+//  Route::get('admin/posts/example',array('as'=>'admin.home',function(){
+//      $url =route('admin.home');
+//      return"this url is ".$url;
 //  }));
 
-// Route::get('post',[PostController::class,'index']);
-Route::get('contact',[PostController::class,'contact']);
-Route::get('post/{id}/{name}/{password}',[PostController::class,'postpage']);
+//  Route::get('nepal/nawalprashi/basa',array('as'=>'puskar.home',function(){
+//     $address=route('puskar.home');
+//     return"puskar lives in".$address." municipality";
+//  }));
 
-Route::get('/insert',function(){
-    DB::insert('insert into posts(title,content)  values(?,?)',['PHP with Laravel','Laravel is a interesting framework']);
-});
+// //  Route::get('admin/post/linkExample',array('as'=>'page.link',function(){
+// //     <a href="route('page.link')">Click Here</a>
+// //  }));
 
+// // Route::get('post',[PostController::class,'index']);
+// Route::get('contact',[PostController::class,'contact']);
+// Route::get('post/{id}/{name}/{password}',[PostController::class,'postpage']);
+
+// Route::get('/insert',function(){
+//     DB::insert('insert into posts(title,content)  values(?,?)',['PHP with Laravel','Laravel is a interesting framework']);
+// });
+
+// Route::get('/read',function(){
+//     $result=DB::select('select * from posts where id=?',[1]);
+//     foreach($result as $post){
+//         return $post->content;
+//     }
+// });
+
+
+// Route::get('/update',function(){
+//     $updated = DB::update('update posts set title="updated-title" where id=?',[1]);
+//     return $updated;
+// });
+
+// Route::get('/delete',function(){
+//     $deleted = DB::delete('delete from posts where id=?',[1]);
+//     return $deleted;
+// });
+
+//extra○t all data
 Route::get('/read',function(){
-    $result=DB::select('select * from posts where id=?',[1]);
-    foreach($result as $post){
-        return $post->content;
+   
+    $posts = Post::all();
+    foreach($posts as $post){
+        echo $post->title;
     }
 });
-
-
-Route::get('/update',function(){
-    $updated = DB::update('update posts set title="updated-title" where id=?',[1]);
-    return $updated;
-});
-
-Route::get('/delete',function(){
-    $deleted = DB::delete('delete from posts where id=?',[1]);
-    return $deleted;
-});
+//same as above
+ Route::get('/data',function(){
+    foreach (Post::all() as $post) {
+        echo $post->title;
+    }
+ });
+//to find the desired data
+ Route::get('/find',function(){
+    $post=post::find(2);
+    return $post->title;
+ });
